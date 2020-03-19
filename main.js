1cae33daf06160058b25f17139fc8abdad26f471
Colin Jaffe <balloonasaurus@gmail.com>
2020-03-19 15:20:59 -0400

    Adds new challenge and rewords directions.

--- main.js
+++ main.js
@@ -1,19 +1,44 @@
-// Global todo list.
+// Make your global todos array here.
 // Put a few todos in there to start with!
-// This is mostly for testing purposes.
+// This is mostly to give us some data to test our functions with.
 
 
-// Print a todo.
-// For now, just console log it!
+// Now test it here just by console.logging a particular todo index.
 
 
-// Print everything on our todo list, INDIVIDUALLY.
-// Make SURE to use the above function!
+// Next: a function that prints a todo.
+// For now, just have this function console log the value passed in!
+// (We'll expand this helper later.)
 
 
-// Add an item to our todo list.
-// Where on the list should we add it?
-// No wrong answer here as long as you can defend the decision!
+
+// Below, call your printTodo with a value of any kind. All it will do is console.log it,
+// but soon we'll do more with it!
+
+// Now write a function that prints everything on our todo list, INDIVIDUALLY.
+// Make SURE to use the above printTodo function!
 
 
-// Remove an item at a given index from our todo list.
+//Now test it out by calling your print-everything function. It should print out each item from your todo list array.
+
+
+// Now please write a function that adds an item to our todo list.
+// Could add it anywhere! We'll talk about where it should be added eventually.
+
+
+// Test the above function by calling it with a value,
+// then calling your print-everything function and making sure it's in there.
+
+
+// Now write a function that removes an item at a given index from our todo list.
+
+
+// Test the above function by passing it an index, then calling our print-everything
+// function to see if the correct thing was removed.
+
+
+// Lastly: write a function that clears all items from our array.
+
+
+// Test it by calling it and then calling your print-everything function.
+// Nothing should print because the array should be empty now!
